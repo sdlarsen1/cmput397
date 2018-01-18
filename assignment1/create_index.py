f171880872f25b12d76b9c82410c0f552597a453
Stephen Larsen <sdlarsen@ualberta.ca>
2018-01-18 12:20:10 -0700

    added super temp schema

--- assignment1/create_index.py
+++ assignment1/create_index.py
@@ -2,14 +2,33 @@ import os
 import sys
 import sqlite3
 
-conn = sqlite3.connect("test.db")  # open / create db
 
-try:
-    directory = sys.argv[1]                  # find and open directory
-    dirList = os.listdir(directory)
+def create_table():
+    try:   # super temporary schema
+        c.execute('''CREATE TABLE inverted_index(
+                        term TEXT,
+                        doc INT,
+                        pos INT)''')
+    except:
+        print("Table already exists, continuing...")
+
+def main():
+    conn = sqlite3.connect("a1.db")  # open / create db
+    c = conn.cursor()
+
+    try:
+        directory = sys.argv[1]                  # find and open directory
+        dirList = os.listdir(directory)
+
+    except:
+        print("Must specify an existing directory.")
+        sys.exit()
+
+    create_table()                         # create the index table
+
     for file in dirList:
         if ".txt" not in file:
             print(file)
-except:
-    print("Must specify an existing directory.")
-    sys.exit()
+
+if __name__ == '__main__':
+    main()
