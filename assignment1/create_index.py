4b6085dd6415d16208c8c1d95d6431e6d2381cfc
Stephen Larsen <sdlarsen@ualberta.ca>
2018-01-18 11:46:32 -0700

    updated gitignore, creat_index parses files in specified directory

--- assignment1/create_index.py
+++ assignment1/create_index.py
@@ -1,4 +1,15 @@
+import os
 import sys
 import sqlite3
 
-conn = sqlite3.connect("test.db")
+conn = sqlite3.connect("test.db")  # open / create db
+
+try:
+    directory = sys.argv[1]                  # find and open directory
+    dirList = os.listdir(directory)
+    for file in dirList:
+        if ".txt" not in file:
+            print(file)
+except:
+    print("Must specify an existing directory.")
+    sys.exit()
