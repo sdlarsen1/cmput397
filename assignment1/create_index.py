9f865fe27d951194fc2d793b3470f98f91394e27
Stephen Larsen <sdlarsen@ualberta.ca>
2018-01-23 11:33:08 -0700

    fixed schema

--- assignment1/create_index.py
+++ assignment1/create_index.py
@@ -4,14 +4,35 @@ import sqlite3
 import nltk
 
 
-def create_table():
-    try:   # super temporary schema
-        c.execute('''CREATE TABLE inverted_index(
-                        term TEXT,
-                        doc_id INT,
-                        pos INT)''')
+def init_db(c):
+    try:
+        # cleanup
+        c.execute("DROP TABLE IF EXISTS token;")
+        c.execute("DROP TABLE IF EXISTS posting;")
+        c.execute("DROP INDEX IF EXISTS token_idx;")
+
+        # start creating tables
+        c.execute('''
+            CREATE TABLE token (
+            token TEXT,
+            token_id INT,
+            PRIMARY KEY(token_id)
+            );''')
+
+        c.execute('''
+            CREATE TABLE posting (
+            token_id INT,
+            doc_id INT,
+            offset INT,
+            FOREIGN KEY (token_id) REFERENCES token(token_id)
+            );''')
+
+        c.execute('''
+            CREATE INDEX token_idx ON token(token);''')
+
     except:
-        print("Table already exists, continuing...")
+        print("Issue setting up database.")
+        sys.exit()
 
 
 def parse_file(filename):
@@ -21,23 +42,24 @@ def parse_file(filename):
         tokens = nltk.word_tokenize(content)
         # do other stuff here
         infile.close()
+
     except:
-        print("Error opening/parsing file.")
+        print("Error opening/parsing file:", filename)
 
 
 def main():
     conn = sqlite3.connect("a1.db")  # open / create db
-    c = conn.cursor()
+    c = conn.cursor()                # set up cursor
 
     try:
         directory = sys.argv[1]                  # find and open directory
         file_list = os.listdir(directory)
 
     except:
-        print("Must specify an existing directory.")
+        print("Must specify an existing directory:", directory)
         sys.exit()
 
-    create_table()                         # create the index table
+    init_db(c)                                # initialize the db
 
     for file in file_list:                   # start parsing directory
         if ".txt" in file:
