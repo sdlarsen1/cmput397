eb234638cc8a0501e3ac6fec891dfaad7bcad132
Stephen Larsen <sdlarsen@ualberta.ca>
2018-01-25 13:27:24 -0700

    renamed test files, implemented porter stemmer

--- assignment1/create_index.py
+++ assignment1/create_index.py
@@ -2,6 +2,8 @@ import os
 import sys
 import sqlite3
 import nltk
+from nltk.stem import *
+from nltk.stem.porter import *
 
 
 def init_db(c):
@@ -36,20 +38,37 @@ def init_db(c):
 
 
 def parse_file(filename):
+    punctuation = [',', '.', ';', ':', "'", '"']
+
     try:
         infile = open(filename, 'r')
         content = infile.read()
-        tokens = nltk.word_tokenize(content)
-        # do other stuff here
-        infile.close()
 
     except:
-        print("Error opening/parsing file:", filename)
+        print("Error opening/reading file:", filename)
+
+    tokens = nltk.word_tokenize(content)
+    stemmer = PorterStemmer()
+    stemmed_tokens = []
+
+    for token in tokens:
+        if token in punctuation:                    # ignore punctuation
+            continue
+
+        token = stemmer.stem(token.lower())         # using Porter stemmer
+        stemmed_tokens.append(token)
+
+    infile.close()
+    return stemmed_tokens
+
+
+def index_tokens(c, tokens, doc_id):
+    return
 
 
 def main():
-    conn = sqlite3.connect("a1.db")  # open / create db
-    c = conn.cursor()                # set up cursor
+    conn = sqlite3.connect("a1.db")     # open / create db
+    c = conn.cursor()                   # set up cursor
 
     try:
         directory = sys.argv[1]                  # find and open directory
@@ -59,12 +78,16 @@ def main():
         print("Must specify an existing directory:", directory)
         sys.exit()
 
-    init_db(c)                                # initialize the db
+    init_db(c)                               # initialize the db
 
     for file in file_list:                   # start parsing directory
         if ".txt" in file:
-            # print(directory+file)
-            parse_file(directory+file)
+            tokens = parse_file(directory+file)
+            # print(tokens)
+            filename = file.split('_')          # filename: doc_#_xyz.txt
+            doc_id = filename[1]
+            index_tokens(c, tokens, doc_id)     # index the file
+
 
 if __name__ == '__main__':
     main()
