23e6111b132899a8da7228adbed8a2250fbb6aa8
Stephen Larsen <sdlarsen@ualberta.ca>
2018-02-08 20:52:03 -0700

    this is before i seriously breka something

--- assignment1/vs_query.py
+++ assignment1/vs_query.py
@@ -9,19 +9,22 @@ from nltk.stem.porter import *
 
 def cosine(query, scores, N, c):
 
-    length = {}             # dict of lengths for each vector
+    magnitude = {}             # used to normalize the scores
 
     for word in query:
-        print("-------"+word+"--------")
+        # print("-------"+word+"--------")
         df_d = df_q = get_doc_frequency(word, c)      # df_q and df_d are the same, but for clarity
         tf_q = get_term_frequency_query(word, query)  # w.r.t. the query
         w_q = tf_idf(tf_q, df_q, N)                   # tf_idf of word in query
 
-        for doc_id in range(N):
-            print("-----"+str(doc_id)+"-----")
+        valid_docs = get_valid_docs(word, c)      # narrow down the docs we need to look at
+        # print(valid_docs)
+
+        for doc_id in valid_docs:
+            # print("-----"+str(doc_id)+"-----")
 
             doc_tokens = get_doc_tokens(doc_id, c)
-            print(doc_tokens)
+            # print(doc_tokens)
 
             length[doc_id] = len(doc_tokens)
 
@@ -34,8 +37,13 @@ def cosine(query, scores, N, c):
             except:
                 scores[doc_id] = w_d * w_q
 
-    for doc_id in range(N):
-        scores[doc_id] /= length[doc_id]
+    # for token in get_all_tokens(c):
+    #     for key in scores.keys():
+    #         tf_token = get_term_frequency(token, )
+    #         magnitude[key] =
+
+    for key in scores.keys():
+        scores[key] /= magnitude[key]
 
     return scores
 
@@ -47,11 +55,11 @@ def tf_idf(tf, df, N):
     else:
         tf_idf = 0
 
-    print("tf =", tf, "df =", df, "tf_idf =", tf_idf)
+    # print("tf =", tf, "df =", df, "tf_idf =", tf_idf)
     return tf_idf
 
 
-def get_number_docs(c):
+def get_number_docs(c):                 # returns number of docs in corpus
     c.execute('''
     SELECT COUNT(DISTINCT doc_id)
     FROM Posting;
@@ -61,7 +69,20 @@ def get_number_docs(c):
     return num[0]
 
 
-def get_doc_tokens(doc_id, c):
+def get_all_tokens(c):                  # returns all tokens in posting list
+    c.execute('''
+    SELECT token
+    FROM Token;
+    ''')
+
+    tokens = []
+    for token in c.fetchall():
+        tokens.append(token[0])
+
+    return tokens
+
+
+def get_doc_tokens(doc_id, c):          # returns all tokens in a specific doc
     c.execute('''
     SELECT t.token
     FROM token t, posting p
@@ -76,7 +97,7 @@ def get_doc_tokens(doc_id, c):
     return tokens
 
 
-def get_doc_frequency(term, c):
+def get_doc_frequency(term, c):         # returns doc freq. of a term
     term = PorterStemmer().stem(term.lower())  # compare stemmed tokens
 
     c.execute('''
@@ -95,7 +116,7 @@ def get_doc_frequency(term, c):
         return freq[0]
 
 
-def get_term_frequency(term, doc_tokens):
+def get_term_frequency(term, doc_tokens):   # returns term frequency
     term = PorterStemmer().stem(term.lower())  # compare stemmed tokens
 
     count = 0
@@ -106,13 +127,30 @@ def get_term_frequency(term, doc_tokens):
     return count
 
 
-def get_term_frequency_query(term, query):
+def get_term_frequency_query(term, query):  # returns term frequency within a query
     count = 0
     for word in query:
         if term == word:
             count += 1
 
     return count
+
+
+def get_valid_docs(term, c):            # returns only docs the term is in
+    term = PorterStemmer().stem(term.lower())  # compare stemmed tokens
+    docs = []
+
+    c.execute('''
+    SELECT DISTINCT p.doc_id
+    FROM posting p, token t
+    WHERE t.token_id = p.token_id
+    AND t.token = ?;
+    ''', (term,))
+
+    for doc_id in c.fetchall():
+        docs.append(doc_id[0])
+
+    return docs
 
 
 def print_k_highest(scores, k, print_scores):
@@ -154,7 +192,6 @@ def main():
 
     N = get_number_docs(c)       # get number of docs in index
     scores = cosine(query, scores, N, c)
-    # print(scores)
 
     #printing happens here
     print_k_highest(scores, k, print_score)
